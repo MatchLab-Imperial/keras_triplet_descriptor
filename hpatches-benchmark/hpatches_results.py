8a3b86cd60824f136b89faf98ddf688ca7ede968
adrian <al4415@ic.ac.uk>
2019-02-15 17:47:11 +0000

    More info added for results and clean patches for generating CSV

--- hpatches-benchmark/hpatches_results.py
+++ hpatches-benchmark/hpatches_results.py
@@ -3,7 +3,7 @@
 Usage:
   hpatches_results.py (-h | --help)
   hpatches_results.py --version
-  hpatches_results.py --descr-name=<>... --task=<>... [--results-dir=<>] [--split=<>] [--pcapl=<>]
+  hpatches_results.py --descr-name=<>... --task=<>... [--results-dir=<>] [--split=<>] [--pcapl=<>] [--more_info]
 
 Options:
   -h --help         Show this screen.
@@ -13,7 +13,7 @@ Options:
   --task=<>         Task name. Valid tasks are {verification,matching,retrieval}.
   --split=<>        Split name. Valid are {a,b,c,full,illum,view}. [default: a]
   --pcapl=<>        Show results for pca-power law descr. [default: no]
-
+  --more_info       Outputs partial scores, not only final mAP
 For more visit: https://github.com/hpatches/
 """
 from utils.tasks import tskdir
@@ -26,16 +26,14 @@ from utils.docopt import docopt
 if __name__ == '__main__':
     opts = docopt(__doc__, version='HPatches 1.0')
     descrs = opts['--descr-name']
-
     with open(os.path.join(tskdir, "splits", "splits.json")) as f:
         splits = json.load(f)
     splt = splits[opts['--split']]
-
     for t in opts['--task']:
         print("%s task results:" % (green(t.capitalize())))
         for desc in descrs:
-            results_methods[t](desc,splt)
+            results_methods[t](desc,splt,opts['--more_info'])
             if opts['--pcapl']!='no':
-                results_methods[t](desc+'_pcapl',splt)
+                results_methods[t](desc+'_pcapl',splt,opts['--more_info'])
             print
         print
--- hpatches-benchmark/utils/results.py
+++ hpatches-benchmark/utils/results.py
@@ -7,22 +7,24 @@ import numpy as np
 
 ft = {'e':'Easy','h':'Hard','t':'Tough'}
 
-def results_verification(desc,splt):
+def results_verification(desc,splt,more_info=False):
     v = {'imbalanced':'ap'}
     res = dill.load(open(os.path.join("results", desc+"_verification_"+splt['name']+".p"), "rb"))
     for r in v:
-        # print("%s - %s variant (%s) " % (blue(desc.upper()),r.capitalize(),v[r]))
+        if more_info:
+            print("%s - %s variant (%s) " % (blue(desc.upper()),r.capitalize(),v[r]))
         heads = ["Noise","Inter","Intra"]
         results = []
         for t in ['e','h','t']:
             results.append([ft[t], res[t]['inter'][r][v[r]],res[t]['intra'][r][v[r]]])
-        # print(tb(results,headers=heads))
+        if more_info:
+            print(tb(results,headers=heads))
     mAP = np.asarray(list(map(lambda x: x[1:], results))).mean()
     print('Mean Average Precision is {:f}'.format(mAP))
 
 
 
-def results_matching(desc,splt):
+def results_matching(desc,splt,more_info=False):
     res = dill.load(open(os.path.join("results", desc+"_matching_"+splt['name']+".p"), "rb"))
     mAP = {'e':0,'h':0,'t':0}
     k_mAP = 0
@@ -33,18 +35,21 @@ def results_matching(desc,splt):
                 mAP[t] += res[seq][t][idx]['ap']
                 k_mAP+=1
     k_mAP = k_mAP / 3.0
-    # print("%s - mAP " % (blue(desc.upper())))
+    if more_info:
+        print("%s - mAP " % (blue(desc.upper())))
 
     results = [mAP['e']/k_mAP,mAP['h']/k_mAP,mAP['t']/k_mAP]
     results.append(sum(results)/float(len(results)))
-    # print(tb([results],headers=heads))
+    if more_info:
+        print(tb([results],headers=heads))
     print('Mean Average Precision is {:f}'.format(results[-1]))
     print("\n")
 
 
-def results_retrieval(desc,splt):
+def results_retrieval(desc,splt,more_info=False):
     res = dill.load(open(os.path.join("results", desc+"_retrieval_"+splt['name']+".p"), "rb"))
-    # print("%s - mAP 10K queries " % (blue(desc.upper())))
+    if more_info:
+        print("%s - mAP 10K queries " % (blue(desc.upper())))
     n_q= float(len(res.keys()))
     heads = ['']
     mAP = dict.fromkeys(['e','h','t'])
@@ -68,7 +73,8 @@ def results_retrieval(desc,splt):
 
     res = np.array(results)[:,1:].astype(np.float32)
     results.append(['mean']+list(np.mean(res,axis=0)))
-    # print(tb(results,headers=heads))
+    if more_info:
+        print(tb(results,headers=heads))
     mAP = np.asarray(results[-1][1:]).mean()
     print('Mean Average Precision is {:f}'.format(mAP))
 
